f93a0f083cd01de9f3f1fd3f76aba815c87bbd63
cleiton <cleiton.leonel@gmail.com>
2021-05-25 15:38:33 -0300

    Correção na estrutura para acertar erros de instalação.

---
+++ support/__main__.py
@@ -0,0 +1,16 @@
+from __future__ import absolute_import
+from support.support import get_platform, resource_path, config, ShareDesktop, ngrok
+
+if __name__ == '__main__':
+    arch_path = '64bits'
+    if get_platform()[1] == 'armv7l':
+        arch_path = 'arm'
+    config.DEFAULT_NGROK_PATH = resource_path(f"bin/{arch_path}/ngrok")
+    desktop = ShareDesktop()
+    try:
+        process = desktop.send_email()
+        process.proc.wait()
+    except KeyboardInterrupt:
+        print("Shutting down server.")
+        ngrok.kill()
+        desktop.kill_threads()
