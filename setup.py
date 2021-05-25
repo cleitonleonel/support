2d11f51e79d3a672916bdf7cc80477e2fe540e78
cleiton <cleiton.leonel@gmail.com>
2021-05-25 12:19:33 -0300

    Adicionado um setup para instalação via pip.

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="support",
+    version="0.0.1",
+    author="Cleiton Leonel Creton",
+    author_email="cleiton.leonel@gmail.com",
+    description="Simple share desktop with python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/cleitonleonel/support",
+    project_urls={
+        "Bug Tracker": "https://github.com/cleitonleonel/support/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
